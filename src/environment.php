6d3af3fa116a794460d41f40c1739645e959c88e
Darnell <collinsdarnell44@gmail.com>
2020-02-11 21:08:44 -0500

    change env variables to not conflict

--- src/environment.php
+++ src/environment.php
@@ -6,17 +6,20 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::create(dirname(__DIR__));
 $dotenv->load();
 $dotenv->required([
-    'MODE',
-    'DISPLAY_ERROR_DETAILS',
-    'LOG_ERRORS',
-    'API',
-    'CLIENT_ID',
-    'CLIENT_SECRET',
+    'WEB_MODE',
+    'WEB_DISPLAY_ERROR_DETAILS',
+    'WEB_LOG_ERRORS',
+    'WEB_SEND_TEST_EMAILS',
+    'WEB_API',
+    'WEB_CLIENT_ID',
+    'WEB_CLIENT_SECRET',
+    'WEB_MAILGUN_API_KEY',
+    'WEB_MAILGUN_DOMAIN',
 ]);
 
 // set constants based on the environemnt we want
 // should be 'dev' or 'prod'
-$mode = getenv('MODE');
+$mode = getenv('WEB_MODE');
 $devMode = true;
 $prodMode = false;
 if ('dev' != $mode) {
--- src/settings.php
+++ src/settings.php
@@ -12,25 +12,25 @@ return function (ContainerBuilder $containerBuilder) {
         'settings' => [
 
             'errorHandlerMiddleware' => [
-                'displayErrorDetails' => filter_var(getenv('DISPLAY_ERROR_DETAILS'), FILTER_VALIDATE_BOOLEAN),
-                'logErrors' => filter_var(getenv('LOG_ERRORS'), FILTER_VALIDATE_BOOLEAN),
-                'logErrorDetails' => filter_var(getenv('LOG_ERRORS'), FILTER_VALIDATE_BOOLEAN),
+                'displayErrorDetails' => filter_var(getenv('WEB_DISPLAY_ERROR_DETAILS'), FILTER_VALIDATE_BOOLEAN),
+                'logErrors' => filter_var(getenv('WEB_LOG_ERRORS'), FILTER_VALIDATE_BOOLEAN),
+                'logErrorDetails' => filter_var(getenv('WEB_LOG_ERRORS'), FILTER_VALIDATE_BOOLEAN),
             ],
 
             // pdo connection to database
             'pdo' => [
-                'connection' => getenv('DB_CONNECTION'),
-                'host' => getenv('DB_HOST'),
-                'port' => getenv('DB_PORT'),
-                'database' => getenv('DB_DATABASE'),
-                'username' => getenv('DB_USERNAME'),
-                'password' => getenv('DB_PASSWORD'),
+                'connection' => getenv('WEB_DB_CONNECTION'),
+                'host' => getenv('WEB_DB_HOST'),
+                'port' => getenv('WEB_DB_PORT'),
+                'database' => getenv('WEB_DB_DATABASE'),
+                'username' => getenv('WEB_DB_USERNAME'),
+                'password' => getenv('WEB_DB_PASSWORD'),
             ],
 
             // API credentials
-            'api' => getenv('API'),
-            'client_id' => getenv('CLIENT_ID'),
-            'client_secret' => getenv('CLIENT_SECRET'),
+            'api' => getenv('WEB_API'),
+            'client_id' => getenv('WEB_CLIENT_ID'),
+            'client_secret' => getenv('WEB_CLIENT_SECRET'),
 
             // monolog logger 
             'logger' => [
@@ -40,9 +40,9 @@ return function (ContainerBuilder $containerBuilder) {
             ],
 
             // mailgun for mail
-            'mailgun_api_key' => getenv('MAILGUN_API_KEY'),
-            'mailgun_domain' => getenv('MAILGUN_DOMAIN'),
-            'mailgun_test_mode' => filter_var(getenv('SEND_TEST_EMAILS'), FILTER_VALIDATE_BOOLEAN), // true means the mail gets sent to mailgun but NOT sent to user
+            'mailgun_api_key' => getenv('WEB_MAILGUN_API_KEY'),
+            'mailgun_domain' => getenv('WEB_MAILGUN_DOMAIN'),
+            'mailgun_test_mode' => filter_var(getenv('WEB_SEND_TEST_EMAILS'), FILTER_VALIDATE_BOOLEAN), // true means the mail gets sent to mailgun but NOT sent to user
         ]
     ]);
 };
